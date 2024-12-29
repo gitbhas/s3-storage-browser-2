18f71e91556c009f2cf583bf06e87cbd823c97cf
gitbhas <36489260+gitbhas@users.noreply.github.com>
2024-12-29 04:40:19 +0000

    modify to use existing s3 bucket

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,9 +1,16 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
-import { storage } from './storage/resource';
+// import { storage } from './storage/resource';
 defineBackend({
   auth,
   data,
-  storage
+  // storage
+});
+
+backend.addOutput({
+  storage: {
+    aws_region: "us-east-1",
+    bucket_name: "ddps-dev-edl-tap"
+  },
 });
