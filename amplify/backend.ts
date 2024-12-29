89529150654a3aa12ca379a7c154305dfe3a6375
gitbhas <36489260+gitbhas@users.noreply.github.com>
2024-12-29 15:56:39 +0000

    rev custom bucket

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,21 +1,21 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
-// import { storage } from './storage/resource';
-// import { myFirstFunction } from './my-first-function/resource'
-// defineBackend({
-//   auth,
-//   data,
-//   storage,
-//   myFirstFunction,
-// });
-const backend = defineBackend({
+import { storage } from './storage/resource';
+import { myFirstFunction } from './my-first-function/resource'
+defineBackend({
   auth,
   data,
+  storage,
+  myFirstFunction,
 });
-backend.addOutput({
-  storage: {
-    aws_region: "us-east-1",
-    bucket_name: "ddps-dev-edl-tap"
-  },
-});
+// const backend = defineBackend({
+//   auth,
+//   data,
+// });
+// backend.addOutput({
+//   storage: {
+//     aws_region: "us-east-1",
+//     bucket_name: "ddps-dev-edl-tap"
+//   },
+// });
