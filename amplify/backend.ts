88b04c258a842b725aa6aa333f2944ee17de850e
gitbhas <36489260+gitbhas@users.noreply.github.com>
2024-12-29 05:27:22 +0000

    revert to amplify bucket

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,20 +1,19 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
-import { defineStorage } from '@aws-amplify/backend';
-// import { storage } from './storage/resource';
-// defineBackend({
-//   auth,
-//   data,
-//   storage
-// });
-const backend = defineBackend({
+import { storage } from './storage/resource';
+defineBackend({
   auth,
   data,
+  storage
 });
-backend.addOutput({
-  storage: {
-    aws_region: "us-east-1",
-    bucket_name: "ddps-dev-edl-tap"
-  },
-});
+// const backend = defineBackend({
+//   auth,
+//   data,
+// });
+// backend.addOutput({
+//   storage: {
+//     aws_region: "us-east-1",
+//     bucket_name: "ddps-dev-edl-tap"
+//   },
+// });
