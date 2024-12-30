506b88a363e3eb5c4ae35deb2b69d7402c9af51a
gitbhas <36489260+gitbhas@users.noreply.github.com>
2024-12-30 21:46:22 +0000

    custom buckets

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,12 +1,12 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
-import { storage } from './storage/resource';
+import { tapbucket } from './storage/resource';
 import { myFirstFunction } from './my-first-function/resource'
 defineBackend({
   auth,
   data,
-  storage,
+  tapbucket,
   myFirstFunction,
 });
 // const backend = defineBackend({
--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,4 +1,14 @@
 import { defineStorage } from '@aws-amplify/backend';
+
+export const tapbucket = defineStorage({
+  name: 'ddps-dev-edl-tap',
+  isDefault: true, // identify your default storage bucket (required)
+  access: (allow) => ({
+    'ACATAP/*': [
+      allow.entity('identity').to(['read', 'write', 'delete'])
+    ]
+  })
+});
 export const storage = defineStorage({
   name: 'myS3Bucket',
   access: (allow) => ({
