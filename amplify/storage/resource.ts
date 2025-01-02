526f34fec7c903474c9f66c194164f8fc24fbaf1
gitbhas <36489260+gitbhas@users.noreply.github.com>
2025-01-02 21:30:29 +0000

    custom s3

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,15 +1,15 @@
 import { defineStorage } from '@aws-amplify/backend';
 
-export const storage = defineStorage({
-  name: 'ddps-dev-airflow',
-  isDefault: true, // identify your default storage bucket (required)
-  access: (allow) => ({
-    'public/*': [
-       allow.guest.to(['read']),
-         allow.authenticated.to(['read', 'write', 'delete']),
-    ]
-  })
-});
+// export const storage = defineStorage({
+//   name: 'ddps-dev-airflow',
+//   isDefault: true, // identify your default storage bucket (required)
+//   access: (allow) => ({
+//     'public/*': [
+//        allow.guest.to(['read']),
+//          allow.authenticated.to(['read', 'write', 'delete']),
+//     ]
+//   })
+// });
 // export const storage = defineStorage({
 //   name: 'ddps-dev-airlow',
 //   access: (allow) => ({
