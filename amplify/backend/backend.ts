a1185741cc4ebddc4dda7320fd32ee0976ee5cde
gitbhas <36489260+gitbhas@users.noreply.github.com>
2025-01-02 16:09:04 +0000

    custom s3

--- amplify/backend/backend.ts
+++ amplify/backend/backend.ts
@@ -1,8 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
-import { storage } from './storage/resource';
-import { myFirstFunction } from './my-first-function/resource'
+import { auth } from '../auth/resource';
+import { data } from '../data/resource';
+import { storage } from '../storage/resource';
+import { myFirstFunction } from '../my-first-function/resource'
 // defineBackend({
 //   auth,
 //   data,
