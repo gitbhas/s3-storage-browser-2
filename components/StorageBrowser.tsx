30243e7f5eb56e470fd7dfa15bc721028d68bc69
gitbhas <36489260+gitbhas@users.noreply.github.com>
2025-01-04 16:31:29 -0500

    Update StorageBrowser.tsx

--- components/StorageBrowser.tsx
+++ components/StorageBrowser.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
-import { createAmplifyAuthAdapter, createStorageBrowser } from '@aws-amplify/ui-react-storage/browser';
+// import { createAmplifyAuthAdapter, createStorageBrowser } from '@aws-amplify/ui-react-storage/browser';
 import '@aws-amplify/ui-react-storage/styles.css';
 import { Amplify } from 'aws-amplify';
 //import * as Auth from "aws-amplify/auth";
 import config from '../amplify_outputs.json';
 //import { CognitoIdentity } from '@aws-sdk/client-cognito-identity';
+import {
+  createStorageBrowser,
+  createAmplifyAuthAdapter,
+  elementsDefault,
+} from "@aws-amplify/ui-react-storage/browser";
 
 // Configure Amplify using the imported configuration
 Amplify.configure(config);
 
-// const defaultPrefixes = [
-//   'public/',
-//   (identityId: string) => `protected/${identityId}/`,
-//   (identityId: string) => `private/${identityId}/`,
-// ];
-// Create the StorageBrowser component with Amplify authentications
-export const { StorageBrowser } = createStorageBrowser(
-  {
-  config: createAmplifyAuthAdapter(),
-  // options: {
-  //   defaultPrefixes
-  // },
+const defaultPrefixes = [
+  'dags/',
+  (identityId: string) => `protected/${identityId}/`,
+  (identityId: string) => `private/${identityId}/`,
+];
+// Create the StorageBrowser component with Amplify authentication
+// export const { StorageBrowser } = createStorageBrowser(
+//   {
+//   config: createAmplifyAuthAdapter(),
+//   options: {
+//     defaultPrefixes
+//   },
+//     });
+export const { StorageBrowser } = createStorageBrowser({
+      elements: elementsDefault, // replace to customize your UI
+      config: createAmplifyAuthAdapter({
+      options: {
+      defaultPrefixes
+        },
+      }),
     });
+ 
+
  
