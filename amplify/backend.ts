d73a722b7df55549857c1150fc92cca5db8179cb
gitbhas <36489260+gitbhas@users.noreply.github.com>
2025-01-04 23:17:15 +0000

    s3 custom revert

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,22 +2,22 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { defineStorage } from '@aws-amplify/backend';
-// import { storage } from './storage/resource';
+import { storage } from './storage/resource';
 import { myFirstFunction } from './my-first-function/resource'
 //reload
-// defineBackend({
-//   auth,
-//   data,
-//   storage,
-//   myFirstFunction,
-// });
-const backend = defineBackend({
+defineBackend({
   auth,
-  data, 
+  data,
+  storage,
+  myFirstFunction,
 });
-backend.addOutput({
-  storage: {
-    aws_region: "us-east-1",
-    bucket_name: "ddps-dev-airflow"
-  },
-});
+// const backend = defineBackend({
+//   auth,
+//   data, 
+// });
+// backend.addOutput({
+//   storage: {
+//     aws_region: "us-east-1",
+//     bucket_name: "ddps-dev-airflow"
+//   },
+// });
--- components/StorageBrowser.tsx
+++ components/StorageBrowser.tsx
@@ -1,29 +1,13 @@
-import {
-  createManagedAuthAdapter,
-  createStorageBrowser,
-  AWSTemporaryCredentials,
-} from '@aws-amplify/ui-react-storage/browser';
-import "@aws-amplify/ui-react-storage/styles.css";
-import { defineAuth, secret } from '@aws-amplify/backend';
+import React from 'react';
+import { createAmplifyAuthAdapter, createStorageBrowser } from '@aws-amplify/ui-react-storage/browser';
+import '@aws-amplify/ui-react-storage/styles.css';
+import { Amplify } from 'aws-amplify';
+import config from '../amplify_outputs.json';
 
+// Configure Amplify using the imported configuration
+Amplify.configure(config);
+
+// Create the StorageBrowser component with Amplify authentication
 export const { StorageBrowser } = createStorageBrowser({
-  config: createManagedAuthAdapter({
-    credentialsProvider: async (options?: { forceRefresh?: boolean }) => {
-      // return your credentials object
-      return {
-        credentials: {
-          accessKeyId: secret('accessKeyId'),
-          secretAccessKey: secret('secretAccessKey'),
-          sessionToken: secret('sessionToken'),
-          expiration: new Date(),
-        },
-      }
-    },
-    // AWS `region` and `accountId`
-    region: 'us-east-1',
-    accountId: '012693954776',
-    // call `onAuthStateChange` when end user auth state changes 
-    // to clear sensitive data from the `StorageBrowser` state
-    registerAuthListener: (onAuthStateChange) => {},
-  })
+  config: createAmplifyAuthAdapter(),
 });
