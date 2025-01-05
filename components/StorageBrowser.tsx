3caee2630abae99586ef9000eae482b06c50acf7
gitbhas <36489260+gitbhas@users.noreply.github.com>
2025-01-05 21:44:29 +0000

    init

--- components/StorageBrowser.tsx
+++ components/StorageBrowser.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { createAmplifyAuthAdapter, createStorageBrowser } from '@aws-amplify/ui-react-storage/browser';
 import '@aws-amplify/ui-react-storage/styles.css';
@@ -12,34 +11,3 @@ Amplify.configure(config);
 export const { StorageBrowser } = createStorageBrowser({
   config: createAmplifyAuthAdapter(),
 });
-=======
-import {
-  createManagedAuthAdapter,
-  createStorageBrowser,
-  AWSTemporaryCredentials,
-} from '@aws-amplify/ui-react-storage/browser';
-import "@aws-amplify/ui-react-storage/styles.css";
-import { defineAuth, secret } from '@aws-amplify/backend';
-
-export const { StorageBrowser } = createStorageBrowser({
-  config: createManagedAuthAdapter({
-    credentialsProvider: async (options?: { forceRefresh?: boolean }) => {
-      // return your credentials object
-      return {
-        credentials: {
-          accessKeyId: secret('accessKeyId'),
-          secretAccessKey: secret('secretAccessKey'),
-          sessionToken: secret('sessionToken'),
-          expiration: new Date(),
-        },
-      }
-    },
-    // AWS `region` and `accountId`
-    region: 'us-east-1',
-    accountId: '012693954776',
-    // call `onAuthStateChange` when end user auth state changes 
-    // to clear sensitive data from the `StorageBrowser` state
-    registerAuthListener: (onAuthStateChange) => {},
-  })
-});
->>>>>>> fced94ab9694c3cd4d81b734d051819d8aeba29c
