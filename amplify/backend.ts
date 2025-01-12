19d8c8a39b5ad60d6341963cdb197be1eb281d42
Murthy Pothapragada <Murthy.Pothapragada@gdit.com>
2025-01-12 18:36:01 -0500

    addln custom bucket

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -15,33 +15,56 @@ const backend = defineBackend({
   auth,
   data 
 });
+// backend.addOutput({
+//   storage: {
+//     aws_region: "us-east-1",
+//     bucket_name: "ddps-dev-airflow",
+//     buckets: [
+//       {
+//         name: "devairflow",
+//         bucket_name: "ddps-dev-airflow",
+//         aws_region: "us-east-1",
+//         paths: {
+//           "dags/*": {
+//             guest: ["get", "list"],
+//             authenticated: ["get", "list", "write"],
+//           },
+//         },
+//       },
+//       {
+//         name: "tstairflow",
+//         bucket_name: "ddps-tst-airflow",
+//         aws_region: "us-east-1",
+//         paths: {
+//           "dags/*": {
+//             guest: ["get", "list"],
+//             authenticated: ["get", "list", "write"],
+//           },
+//         },
+//       },
+//     ],
+//   },
+// });
 backend.addOutput({
   storage: {
     aws_region: "us-east-1",
-    bucket_name: "ddps-dev-airflow",
+    bucket_name: "ddps-test2-edl-tap",
     buckets: [
       {
-        name: "devairflow",
-        bucket_name: "ddps-dev-airflow",
+        name: "tst2tap",
+        bucket_name: "ddps-test2-edl-tap",
         aws_region: "us-east-1",
         paths: {
-          "dags/*": {
+          "metadata/*": {
+            guest: ["get", "list"],
+            authenticated: ["get", "list", "write"],
+          },
+          "TAP/*": {
             guest: ["get", "list"],
             authenticated: ["get", "list", "write"],
           },
         },
-      },
-      {
-        name: "tstairflow",
-        bucket_name: "ddps-tst-airflow",
-        aws_region: "us-east-1",
-        paths: {
-          "dags/*": {
-            guest: ["get", "list"],
-            authenticated: ["get", "list", "write"],
-          },
-        },
-      },
+      }
     ],
   },
 });
