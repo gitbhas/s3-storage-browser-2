99eec5d8c1a23aff7f14411d92fa2280dd5a31b5
gitbhas <36489260+gitbhas@users.noreply.github.com>
2025-02-04 16:18:53 -0500

    Update resource.ts

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,16 +1,26 @@
+// import { defineStorage } from '@aws-amplify/backend';
+// export const storage = defineStorage({
+//   name: 'ddps-dev-ui',
+//   access: (allow) => ({
+//     'public/*': [
+//       allow.guest.to(['read']),
+//       allow.authenticated.to(['read', 'write', 'delete']),
+//     ],
+//     'protected/{entity_id}/*': [
+//       allow.authenticated.to(['read']),
+//       allow.entity('identity').to(['read', 'write', 'delete'])
+//     ],
+//     'private/{entity_id}/*': [
+//       allow.entity('identity').to(['read', 'write', 'delete'])
+//     ]
+//   })
+// });
 import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'ddps-dev-ui',
   access: (allow) => ({
-    'public/*': [
-      allow.guest.to(['read']),
-      allow.authenticated.to(['read', 'write', 'delete']),
-    ],
-    'protected/{entity_id}/*': [
+     'protected/{entity_id}/*': [
       allow.authenticated.to(['read']),
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ],
-    'private/{entity_id}/*': [
       allow.entity('identity').to(['read', 'write', 'delete'])
     ]
   })
