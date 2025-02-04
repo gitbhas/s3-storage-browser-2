a4bb37258c1b6d9feeec0dee0036bc5f4cd6ead6
gitbhas <36489260+gitbhas@users.noreply.github.com>
2025-02-04 16:46:26 -0500

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,21 +1,50 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-// import { defineStorage } from '@aws-amplify/backend';
+//import { defineStorage } from '@aws-amplify/backend';
 //import { storage } from './storage/resource';
-//import { myFirstFunction } from './my-first-function/resource'
-//reload
+import { myFirstFunction } from './my-first-function/resource'
+// //reload
 // defineBackend({
 //   auth,
 //   data,
-//   storage
-//   //myFirstFunction,
+//   storage,
+//   myFirstFunction,
 // });
 const backend = defineBackend({
   auth,
   data 
 });
-
+// backend.addOutput({
+//   storage: {
+//     aws_region: "us-east-1",
+//     bucket_name: "ddps-dev-airflow",
+//     buckets: [
+//       {
+//         name: "devairflow",
+//         bucket_name: "ddps-dev-airflow",
+//         aws_region: "us-east-1",
+//         paths: {
+//           "dags/*": {
+//             guest: ["get", "list"],
+//             authenticated: ["get", "list", "write"],
+//           },
+//         },
+//       },
+//       {
+//         name: "tstairflow",
+//         bucket_name: "ddps-tst-airflow",
+//         aws_region: "us-east-1",
+//         paths: {
+//           "dags/*": {
+//             guest: ["get", "list"],
+//             authenticated: ["get", "list", "write"],
+//           },
+//         },
+//       },
+//     ],
+//   },
+// });
 backend.addOutput({
   storage: {
     aws_region: "us-east-1",
@@ -26,7 +55,11 @@ backend.addOutput({
         bucket_name: "ddps-test2-edl-tap",
         aws_region: "us-east-1",
         paths: {
-           "TAP/*": {
+          "metadata/*": {
+            guest: ["get", "list"],
+            authenticated: ["get", "list", "write"],
+          },
+          "TAP/*": {
             guest: ["get", "list"],
             authenticated: ["get", "list", "write"],
           },
