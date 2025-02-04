2330fb4e352fef12b514a45c0fffaabda8a5c2db
gitbhas <36489260+gitbhas@users.noreply.github.com>
2025-02-04 16:26:41 -0500

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,36 +2,36 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { defineStorage } from '@aws-amplify/backend';
-import { storage } from './storage/resource';
+//import { storage } from './storage/resource';
 //import { myFirstFunction } from './my-first-function/resource'
 //reload
-defineBackend({
-  auth,
-  data,
-  storage
-  //myFirstFunction,
-});
-// const backend = defineBackend({
+// defineBackend({
 //   auth,
-//   data 
+//   data,
+//   storage
+//   //myFirstFunction,
 // });
+const backend = defineBackend({
+  auth,
+  data 
+});
 
-// backend.addOutput({
-//   storage: {
-//     aws_region: "us-east-1",
-//     bucket_name: "ddps-test2-edl-tap",
-//     buckets: [
-//       {
-//         name: "tst2tap",
-//         bucket_name: "ddps-test2-edl-tap",
-//         aws_region: "us-east-1",
-//         paths: {
-//            "{entity_id}/*": {
-//             guest: ["get", "list"],
-//             authenticated: ["get", "list", "write"],
-//           },
-//         },
-//       }
-//     ],
-//   },
-// });
+backend.addOutput({
+  storage: {
+    aws_region: "us-east-1",
+    bucket_name: "ddps-test2-edl-tap",
+    buckets: [
+      {
+        name: "tst2tap",
+        bucket_name: "ddps-test2-edl-tap",
+        aws_region: "us-east-1",
+        paths: {
+           "TAP/{entity_id}/*": {
+            guest: ["get", "list"],
+            authenticated: ["get", "list", "write"],
+          },
+        },
+      }
+    ],
+  },
+});
