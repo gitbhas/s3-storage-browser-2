1f97cb0a33707c3c9cf7b2b5a69cf1e6de8e19ac
gitbhas <36489260+gitbhas@users.noreply.github.com>
2025-02-05 13:21:55 -0500

    Update resource.ts

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -20,7 +20,7 @@ export const storage = defineStorage({
   name: 'ddps-dev-ui',
   access: (allow) => ({
      'upload/{entity_id}/*': [
-      allow.authenticated.to(['read'])
+      allow.authenticated.to(['read']),
       allow.entity('identity').to(['read', 'write', 'delete'])
     ]
   })
