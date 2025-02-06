b308abc28ca5dd5f91a5dec31d52368c562298b8
gitbhas <36489260+gitbhas@users.noreply.github.com>
2025-02-05 21:59:00 -0500

    Update resource.ts

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,4 +1,4 @@
-import { defineStorage } from '@aws-amplify/backend';
+// import { defineStorage } from '@aws-amplify/backend';
 // export const storage = defineStorage({
 //   name: 'ddps-dev-ui',
 //   access: (allow) => ({
@@ -15,13 +15,13 @@ import { defineStorage } from '@aws-amplify/backend';
 //     ]
 //   })
 // });
-import { defineStorage } from '@aws-amplify/backend';
-export const storage = defineStorage({
-  name: 'ddps-dev-ui',
-  access: (allow) => ({
-     'upload/{entity_id}/*': [
-      allow.authenticated.to(['read']),
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ]
-  })
-});
+// import { defineStorage } from '@aws-amplify/backend';
+// export const storage = defineStorage({
+//   name: 'ddps-dev-ui',
+//   access: (allow) => ({
+//      'upload/{entity_id}/*': [
+//       allow.authenticated.to(['read']),
+//       allow.entity('identity').to(['read', 'write', 'delete'])
+//     ]
+//   })
+// });
