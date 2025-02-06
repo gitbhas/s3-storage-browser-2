7584c1c4aa3f0f954db3cc6ea019c8149c2dd150
gitbhas <36489260+gitbhas@users.noreply.github.com>
2025-02-05 21:33:50 -0500

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,35 +3,35 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { defineStorage } from '@aws-amplify/backend';
 import { Amplify} from 'aws-amplify';
-import { storage } from './storage/resource';
-import { myFirstFunction } from './my-first-function/resource'
+//import { storage } from './storage/resource';
+//import { myFirstFunction } from './my-first-function/resource'
 // //reload
-defineBackend({
-  auth,
-  data,
-  storage,
-  myFirstFunction,
-});
-// const backend = defineBackend({
+// defineBackend({
 //   auth,
-//   data 
+//   data,
+//   storage,
+//   myFirstFunction,
 // });
-// backend.addOutput({
-//   storage: {
-//     aws_region: "us-east-1",
-//     bucket_name: "ddps-test2-edl-tap",
-//     buckets: [
-//       {
-//         name: "340b",
-//         bucket_name: "ddps-340b-upload",
-//         aws_region: "us-east-1",
-//         paths: { 
-//           "landing/*": {
-//             guest: ["get", "list"],
-//             authenticated: ["get", "list", "write"],
-//           },
-//         },
-//       }
-//     ],
-//   },
-// });
+const backend = defineBackend({
+  auth,
+  data 
+});
+backend.addOutput({
+  storage: {
+    aws_region: "us-east-1",
+    bucket_name: "ddps-test2-edl-tap",
+    buckets: [
+      {
+        name: "340b",
+        bucket_name: "ddps-340b-upload",
+        aws_region: "us-east-1",
+        paths: { 
+          "landing/*": {
+            guest: ["get", "list"],
+            authenticated: ["get", "list", "write"],
+          },
+        },
+      }
+    ],
+  },
+});
