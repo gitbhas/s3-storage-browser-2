875431283ebf69f0874e37e3866d5ebecbee4ee1
gitbhas <36489260+gitbhas@users.noreply.github.com>
2025-02-06 00:20:14 -0500

    Update resource.ts

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,10 +1,10 @@
 import { defineStorage } from '@aws-amplify/backend';
-const storage = defineStorage({
-  name: 'storage', // Give your storage stack a name
-  bucket: {
-    name: 'ddps-test2-edl-tap' // Your bucket name
-  }
-});
+// const storage = defineStorage({
+//   name: 'storage', // Give your storage stack a name
+//   bucket: {
+//     name: 'ddps-test2-edl-tap' // Your bucket name
+//   }
+// });
 // export const storage = defineStorage({
 //   name: 'ddps-dev-ui',
 //   access: (allow) => ({
@@ -21,13 +21,13 @@ const storage = defineStorage({
 //     ]
 //   })
 // });
-// import { defineStorage } from '@aws-amplify/backend';
-// export const storage = defineStorage({
-//   name: 'ddps-dev-ui',
-//   access: (allow) => ({
-//      'upload/{entity_id}/*': [
-//       allow.authenticated.to(['read']),
-//       allow.entity('identity').to(['read', 'write', 'delete'])
-//     ]
-//   })
-// });
+import { defineStorage } from '@aws-amplify/backend';
+export const storage = defineStorage({
+  name: 'ddps-dev-upload',
+  access: (allow) => ({
+     'upload/{entity_id}/*': [
+      allow.authenticated.to(['read']),
+      allow.entity('identity').to(['read', 'write', 'delete'])
+    ]
+  })
+});
