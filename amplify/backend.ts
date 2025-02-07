2d603299c7b4ce44c4621abfd605f44baaf89494
gitbhas <36489260+gitbhas@users.noreply.github.com>
2025-02-07 00:06:01 -0500

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -28,7 +28,7 @@ backend.addOutput({
         bucket_name: "ddps-340b-upload",
         aws_region: "us-east-1",
         paths: { 
-          "landing/{custom:entity_id}/*": {
+          "landing/*": {
             guest: ["get", "list"],
             authenticated: ["get", "list", "write"],
           },
