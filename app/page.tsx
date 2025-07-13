32225e22fed59f633300d6f6ff70fd590e7d66ef
Murthy Pothapragada <murthy.pothapragada@gdit.com>
2025-07-13 14:49:10 -0400

    new  message

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ export default function App() {
             Hello {userData?.preferred_username || user?.username || 'User'}
           </h1>
           <button onClick={signOut}>Sign out</button>
-          <h2>DDPS Files</h2>
+          <h2>PDE Data</h2>
           <StorageBrowser />
         </main>
       )}
